31f51eda57691caad4192c079793beb532cc5cf2
[Hugo] <[hugosnts.sousa@gmail.com]>
2024-09-03 22:11:20 -0700

    swap variable values program

---
+++ swap_numbers.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+    int num1 = 2;
+    int num2 = 1;
+    int swap;
+
+    swap = num1;
+    num1 = num2;
+    num2 = swap;
+
+    cout << "Hello\n"<<num1<< " "<< num2;
+
+
+}
